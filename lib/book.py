a9a9f55ece3f5c1b2c8a6820e941531de9d34864
florence wangechi <florencewangechim@gmail.com>
2023-05-23 20:09:31 +0300

    Complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
+ 
+
 class Book:
-    pass
+
+    def __init__(self,title,page_count):
+        self.title=title
+
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+book=Book("And Then There Were None",272)
+print(book.title) 
+print(book.page_count)
+book.turn_page()     
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand=brand
+        self.condition = 'New'
+        if not isinstance(size,int):
+            print("size must be an integer")
+        else:
+            self.size=size
+
+    def repair(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+        
+shoe=Shoe("Adidas",9)
+print(shoe.brand)
+print(shoe.size)
+shoe.repair()
